2a14130539e8ea1b5a9afdaa34e543f0d718170b
MukalDadhwal <mukaldadhwal@gmail.com>
2026-01-12 17:43:26 +0530

    intial commit

---
+++ lib/features/recipes/presentation/pages/recipe_list_page.dart
@@ -0,0 +1,13 @@
+class RecipeListPage extends StatefulWidget {
+  const RecipeListPage({super.key});
+
+  @override
+  State<RecipeListPage> createState() => _RecipeListPageState();
+}
+
+class _RecipeListPageState extends State<RecipeListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
